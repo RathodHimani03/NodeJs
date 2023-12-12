d3df188ccce61710219d788d552783c8c6aef0db
pp-3 <himani.r@prominentpixel.com>
2023-12-12 12:20:08 +0530

    Working express js module

--- module_05/ExpressJs/Routes/shop.js
+++ module_05/ExpressJs/Routes/shop.js
@@ -1,12 +1,14 @@
+const path = require('path');
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
 
 
-router.get('/',(req,res,next)=>{   
-
-    res.send('<h1> Hello from Everyone </h1>')
-})
-
-
-module.exports = router
+module.exports = router;
--- module_05/ExpressJs/app.js
+++ module_05/ExpressJs/app.js
@@ -1,17 +1,21 @@
+const path = require('path');
 
-const express = require('express')
-const bodyParse = require('body-parser')
-const adminRoutes = require('./Routes/admin')
-const shopRouter = require('./Routes/shop')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(bodyParse.urlencoded({extended:false}))
+const app = express();
 
-app.use(adminRoutes)
-app.use(shopRouter)
+const adminRoutes = require('./Routes/admin');
+const shopRoutes = require('./Routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-
-app.listen(3000)
+app.listen(3000);
